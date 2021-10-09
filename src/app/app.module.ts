7b57ae5f8179225f083bce0d62826cd10f40ab1e
durga <durga.nskumar@gmail.com>
2021-10-09 11:25:00 +0400

    version 1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SampleComponent } from './components/sample/sample.component';
+import { SampledemoComponent } from './components/sampledemo/sampledemo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponent,
+    SampledemoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/sampledemo/sampledemo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sampledemo',
+  templateUrl: './sampledemo.component.html',
+  styleUrls: ['./sampledemo.component.css']
+})
+export class SampledemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
